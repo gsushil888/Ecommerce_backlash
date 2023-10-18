19765683dcd61e75f90f97ceb95973650ae0f0a0
gsushil888 <tmail7458@gmail.com>
2023-10-18 14:10:24 +0530

    Ecommerce Backlash Programming

---
+++ src/main/java/com/backlashprogramming/ecommerce/EcommerceByBacklash/EcommerceByBacklashApplication.java
@@ -0,0 +1,13 @@
+package com.backlashprogramming.ecommerce.EcommerceByBacklash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceByBacklashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceByBacklashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backlashprogramming/ecommerce/EcommerceByBacklash/EcommerceByBacklashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backlashprogramming.ecommerce.EcommerceByBacklash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceByBacklashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
